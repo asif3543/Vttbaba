1cbf830d362db37fe1de9f572903538cd1736ca5
asif3543 <ikhanking38@gmail.com>
2026-04-01 23:43:07 +0530

    Refactor environment variable retrieval in config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os
 
-API_ID = int(os.environ.get("API_ID", 0))
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID = int(os.getenv("API_ID", 57785446)) 
+API_HASH = os.getenv("API_HASH", "YOUR_API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN")
 
-SUPABASE_URL = os.environ.get("SUPABASE_URL", "")
-SUPABASE_KEY = os.environ.get("SUPABASE_KEY", "")
+SUPABASE_URL = os.getenv("SUPABASE_URL", "https://dxvnreuovwoncgonbggg.supabase.co")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY", "YOUR_SUPABASE_KEY")
 
-OWNER_ID = int(os.environ.get("OWNER_ID", 5351848105))
-ALLOWED_USER = int(os.environ.get("ALLOWED_USER", 5344078567))
-STORAGE_CHANNEL_ID = int(os.environ.get("STORAGE_CHANNEL_ID", -1003096528862))
-PORT = int(os.environ.get("PORT", 10000))
+OWNER_ID = int(os.getenv("OWNER_ID", 5351848105))
+ALLOWED_USER = int(os.getenv("ALLOWED_USER", 5344078567))
+ALLOWED_GROUP = int(os.getenv("ALLOWED_GROUP", -1003899919015))
+STORAGE_CHANNEL_ID = int(os.getenv("STORAGE_CHANNEL_ID", -1003096528862))
 
-# Jo log bot use kar sakte hain admin commands ke liye
-ADMINS = [OWNER_ID, ALLOWED_USER]
+# Sirf ye log/groups bot use kar payenge Admin mode me
+ADMINS = [OWNER_ID, ALLOWED_USER, ALLOWED_GROUP]

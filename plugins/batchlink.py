2ecc34b4d0685c2bd574ad3c08e659077ced847a
asif3543 <ikhanking38@gmail.com>
2026-04-02 13:40:21 +0530

    Delete plugins directory

--- plugins/batchlink.py
+++
@@ -1,6 +0,0 @@
-from pyrogram import filters
-
-def register(app):
-    @app.on_message(filters.command("batchlink"))
-    async def batchlink_cmd(client, message):
-        await message.reply_text("Send the episode range for batch posting.")

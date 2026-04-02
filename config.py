ebe91bc0bdc93246701a7166ef0a40e84c017f75
asif3543 <ikhanking38@gmail.com>
2026-04-02 21:45:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,22 @@
 import os
 
 class Config:
-    # Render Environment Variables se aayenge
+    # Render se auto fetch hoga
     API_ID = int(os.environ.get("API_ID", "0"))
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-    # Hardcoded Database & Keys (Aapki di hui)
+    # Hardcoded Database
     SUPABASE_URL = "https://dxvnreuovwoncgonbggg.supabase.co"
     SUPABASE_KEY = "sb_publishable_jNeOKE9aANf2hVZtEiB9dQ_YCinuxVP"
 
-    # Security Setup
+    # Security & Admin Setup
     OWNER_ID = 5351848105
-    ALLOWED_USERS = [5344078567]
-    ALLOWED_GROUP = -1003899919015 # Apna allowed group ID yaha dal lein agar koi specific hai
-    
-    # Storage Channel Jaha se files forward hongi
+    ALLOWED_USERS = [5344078567, 5351848105]
+    ALLOWED_GROUP = -1003899919015
+
+    # Storage Channel (ASI Anime - jaha episodes save honge)
     STORAGE_CHANNEL = -1003143681742
     
-    # Render Keep-Alive Port
+    # Port for Render
     PORT = 10000

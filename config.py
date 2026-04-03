576d60326f372aa0d76bf6d8fe88f820bf9ae592
asif3543 <ikhanking38@gmail.com>
2026-04-03 08:37:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,18 @@
 import os
 
 class Config:
-
-    API_ID = int(os.getenv("API_ID"))
-    API_HASH = os.getenv("API_HASH")
-    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    API_ID = int(os.getenv("API_ID", "0"))
+    API_HASH = os.getenv("API_HASH", "")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 
     OWNER_ID = 5351848105
-    ALLOWED_USERS = [5344078567]
+    ALLOWED_USERS = [5344078567, 5351848105]
 
     STORAGE_CHANNEL = -1003096528862
     ALLOWED_GROUP = -1003899919015
 
-    # ✅ Hardcoded Supabase
     SUPABASE_URL = "https://dxvnreuovwoncgonbggg.supabase.co"
     SUPABASE_KEY = "sb_publishable_jNeOKE9aANf2hVZtEiB9dQ_YCinuxVP"
 
-    TEMP = {}
+    # Centralized Dictionary for User States
+    STATE = {}

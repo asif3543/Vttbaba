3ac0ff012d7c2bae7fd93a21be3e39e0dfb433d2
asif3543 <ikhanking38@gmail.com>
2026-04-03 08:12:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,14 @@
-import logging
-import threading
 from pyrogram import Client
-from flask import Flask
 from config import Config
 
-logging.basicConfig(level=logging.INFO)
+app = Client(
+    "bot",
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    bot_token=Config.BOT_TOKEN,
+    plugins={"root": "plugins"} #yaha se files lega 
+)
 
-app = Flask(__name__)
+print("🚀 Bot Started on PORT 10000")
 
-@app.route('/')
-def home():
-    return "File Store Bot is Live and Running!"
-
-def run_flask():
-    app.run(host="0.0.0.0", port=Config.PORT)
-
-class FileStoreBot(Client):
-    def __init__(self):
-        super().__init__(
-            "FileStoreBot",
-            api_id=Config.API_ID,
-            api_hash=Config.API_HASH,
-            bot_token=Config.BOT_TOKEN,
-            plugins=dict(root="plugins")
-        )
-
-    async def start(self):
-        await super().start()
-        me = await self.get_me()
-        print(f"✅ Bot Started Successfully: @{me.username}")
-
-if __name__ == "__main__":
-    threading.Thread(target=run_flask, daemon=True).start()
-    FileStoreBot().run()
+app.run()

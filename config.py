2c9ca34fe6e9422c0c7756b7636efe5c6dc13759
asif3543 <ikhanking38@gmail.com>
2026-04-03 21:21:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,17 @@ API_ID = int(os.environ.get("API_ID", "0"))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
+# 👑 OWNER & ADMIN CONTROLS
 OWNER_ID = 5351848105
 ADMINS =[5351848105, 5344078567]
 
+# 📺 CHANNELS
 STORAGE_CHANNEL_ID = -1003096528862
 ALLOWED_GROUP = -1003899919015
 
+# 🗄️ SUPABASE DATABASE
 SUPABASE_URL = "https://dxvnreuovwoncgonbggg.supabase.co"
 SUPABASE_KEY = "sb_publishable_jNeOKE9aANf2hVZtEiB9dQ_YCinuxVP"
 
-# Temporary memory to store workflow state across all plugins
+# 🧠 MEMORY (सभी प्लगइन्स इसे यूज़ करेंगे)
 USER_STATE = {}

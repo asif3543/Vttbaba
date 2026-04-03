6c38d30abc2ebdc3abf187886cd273ce6e4f85e4
asif3543 <ikhanking38@gmail.com>
2026-04-03 08:13:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,19 @@
 import os
 
 class Config:
-    # Render se auto fetch hoga
-    API_ID = int(os.environ.get("API_ID", "0"))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-    # Hardcoded Database
+    API_ID = int(os.getenv("API_ID"))
+    API_HASH = os.getenv("API_HASH")
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+    OWNER_ID = 5351848105
+    ALLOWED_USERS = [5344078567]
+
+    STORAGE_CHANNEL = -1003096528862
+    ALLOWED_GROUP = -1003899919015
+
+    # ✅ Hardcoded Supabase
     SUPABASE_URL = "https://dxvnreuovwoncgonbggg.supabase.co"
     SUPABASE_KEY = "sb_publishable_jNeOKE9aANf2hVZtEiB9dQ_YCinuxVP"
 
-    # Security & Admin Setup
-    OWNER_ID = 5351848105
-    ALLOWED_USERS = [5344078567, 5351848105]
-    ALLOWED_GROUP = -1003899919015
-
-    # Storage Channel (ASI Anime - jaha episodes save honge)
-    STORAGE_CHANNEL = -1003143681742
-    
-    # Port for Render
-    PORT = 10000
+    TEMP = {}

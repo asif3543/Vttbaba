4a3003a4d0a24cb0c3411b4ed42970980262a346
asif3543 <ikhanking38@gmail.com>
2026-04-04 23:28:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,14 @@
 import os
 
-# Bot Configuration
-BOT_TOKEN = "8397073883:AAEUc_qW_dzmxBzrHZehoczWl3MFY0Z4WNM"  # 🔥 Apna bot token yahan daal
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
 
-API_ID = "35178818"  # 🔥 Apna API ID daal
-API_HASH = "d43a4cea9d57e2fe1151fd36c4792576"  # 🔥 Apna API Hash daal
+OWNER_ID = int(os.getenv("OWNER_ID", 5351848105))
+ALLOWED_USERS = [int(x) for x in os.getenv("ALLOWED_USERS", "5344078567").split(",")]
+ALLOWED_GROUPS = [int(x) for x in os.getenv("ALLOWED_GROUPS", "-1003899919015").split(",")]
 
-# Owner & Allowed Users
-OWNER_ID = 5351848105  # 🔥 Apna owner ID daal
-ALLOWED_USERS = [5344078567]  # 🔥 Allowed user IDs list mein daal
-ALLOWED_GROUPS = [-1003899919015]  # 🔥 Allowed group IDs
+STORAGE_CHANNEL_ID = int(os.getenv("STORAGE_CHANNEL_ID", -1003096528862))
 
-# Storage Channel (Private channel where bot stores posts)
-STORAGE_CHANNEL_ID = -1003096528862  # 🔥 Apna storage channel ID daal
-
-# MongoDB URL (Seedha code mein daal diya)
-MONGODB_URL = "mongodb+srv://aasifhusenaasifkhan_db_user:64CtKuQjWL0EzYMO@botcluster.v4land1.mongodb.net/?retryWrites=true&w=majority&appName=BotCluster"
-
-# Database Name
-DATABASE_NAME = "telegram_bot"
+MONGODB_URL = os.getenv("MONGODB_URL")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "telegram_bot")

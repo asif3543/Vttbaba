97ef5201158ca032ed3eb5ddd3624ce2822cc9c8
asif3543 <ikhanking38@gmail.com>
2026-04-04 07:37:06 +0530

    Update post.py

--- plugins/post.py
+++ plugins/post.py
@@ -1,4 +1,4 @@
-herefrom pyrogram import Client, filters
+from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from config import ADMINS, USER_STATE
 from database import add_post, add_batch

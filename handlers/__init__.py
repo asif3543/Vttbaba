0c57f0db0f56cbcabc915ac3991b0dba7ebf8112
asif3543 <ikhanking38@gmail.com>
2026-04-05 13:30:18 +0530

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,16 @@
+from .start import router as start_router
+from .post import router as post_router
+from .send import router as send_router
+from .shortner import router as shortner_router
+from .premium import router as premium_router
+from .forcesub import router as fsub_router
+
+# Merge all routers
+from aiogram import Router
+router = Router()
+router.include_router(start_router)
+router.include_router(post_router)
+router.include_router(send_router)
+router.include_router(shortner_router)
+router.include_router(premium_router)
+router.include_router(fsub_router)
